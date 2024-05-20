c97fc5b96c918aac4ddb0ce7132724dfa998c30a
MSI <othmanebkk4@gmail.com>
2024-05-20 20:17:13 +0200

    Pour connaitre l'adresse Mac de l'ESP

---
+++ sketch_MAC_Adr/sketch_MAC_Adr.ino
@@ -0,0 +1,13 @@
+#include <WiFi.h>
+
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  
+}
+ 
+void loop(){
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
